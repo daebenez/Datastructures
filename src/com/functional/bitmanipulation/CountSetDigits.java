04b3f2bf030ba493ea676728de1db26078b6044f
daebenez <davidebenezer.me@gmail.com>
2017-02-24 00:14:53 -0600

    Added stackoverflow link for example

--- src/com/functional/bitmanipulation/CountSetDigits.java
+++ src/com/functional/bitmanipulation/CountSetDigits.java
@@ -4,6 +4,7 @@
  * When n is & with (n-1) rightmost set bit is cleared and counted, this way we count all set bits in n
  * Time complexity is O(log n) | because worst case n may have all its bit set to 1 in which case this
  * this algorithm sets every bit to 0 before counting it.
+ * For example see here : http://stackoverflow.com/questions/12383002/please-explain-the-logic-behind-kernighans-bit-counting-algorithm
  */
 
 package com.ds.simple;
