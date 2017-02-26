75d1d4f126ca113a408c95154c7c640c61d4a844
daebenez <davidebenezer.me@gmail.com>
2017-02-26 00:33:30 -0600

    Added time complexity and additional explanation for GCD.

--- src/com/functional/recursion/GCD.java
+++ src/com/functional/recursion/GCD.java
@@ -2,7 +2,9 @@ package com.functional.recursion;
 /*
  * @author daebenez
  * Program to find GCD of a number using recursion
- * Euclidean formula
+ * Euclidean formula - If the reminder of the higher number divided by the lower number is 0
+ * the lower number is the GCD. If not recurively divide the lower number by the remainder.
+ * Time complexity : O(log n) --> https://www.youtube.com/watch?v=7HCd074v8g8	 
  */
 public class GCD {
 	
