17a688cdaa7bd95ff72fb7cb4dfd7981e34247d7
David Ebenezer <David Ebenezer>
2017-06-10 19:11:34 -0500

    insert a binary digit into a larger binary number

---
+++ src/com/datastructure/bitmanipulation/insertBits.java
@@ -0,0 +1,33 @@
+package com.datastructure.bitmanipulation;
+/**
+ * Q 5.1
+ * Program to insert an integer into another using Bit manipulation technique.
+ * @author daebenez
+ * Time complexity : O(log N) -> iterating through binary representation of an integer takes this time.
+ */
+public class insertBits {
+	
+	public void insert(int N,int M,int i,int j)
+	{
+		//prepare mask for N
+		int allOnes = ~0;
+		// Shift all ones to left j+1 times to create trailing zeros at the end
+		int rightMask = allOnes << (j+1);
+		int leftMask = allOnes << (i);
+		leftMask = ~leftMask;
+		int mask = rightMask | leftMask;
+		//set bits from i to j to zero
+		int N_cleared = N & mask;
+		// trailing zeros for N
+		M = M << i;
+		int result = N_cleared | M;
+		System.out.println(result);
+	}
+	/*
+	public static void main(String[] args)
+	{
+		insertBits obj = new insertBits();
+		obj.insert(512, 19, 2, 6);
+	} */
+
+}
